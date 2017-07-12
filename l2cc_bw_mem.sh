7e31164fbcce84251bb3652159fb9abf6b6a78fc
Wenyou Yang <wenyou.yang@microchip.com>
2017-07-12 13:12:19 +0800

    script: Add test script for l2cc
    Signed-off-by: Wenyou Yang <wenyou.yang@microchip.com>

---
+++ l2cc_bw_mem.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+bw_mem 16m rdwr
+bw_mem 8m rdwr
+bw_mem 4m rdwr
+bw_mem 2m rdwr
+bw_mem 1m rdwr
+bw_mem 512k rdwr
+bw_mem 256k rdwr
+bw_mem 128k rdwr
+bw_mem 64k rdwr
+bw_mem 32k rdwr
+bw_mem 16k rdwr
+bw_mem 8k rdwr
+bw_mem 4k rdwr
+bw_mem 2k rdwr
+bw_mem 1k rdwr
+bw_mem 512 rdwr
+
